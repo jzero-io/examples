448df322ea3dd49433ff9acd84741a7a71c02d16
jaron <jaron@jaronnie.com>
2024-07-29 08:44:28 +0000

    chore(quickstart): update quickstart

---
+++ quickstart/quickstart-go/typed/quickstart/quickstart_expansion.go
@@ -0,0 +1,3 @@
+package quickstart
+
+type VersionExpansion interface {}
---
+++ quickstart/zrpcclient-go/typed/quickstart/quickstart_client.go
@@ -0,0 +1,27 @@
+package quickstart
+
+import (
+	"github.com/zeromicro/go-zero/zrpc"
+	
+	"quickstart/zrpcclient-go/typed/quickstart/hello"
+	
+)
+
+type Interface interface {
+	
+	Hello() hello.Hello
+	
+}
+
+type Client struct {
+	client zrpc.Client
+}
+
+func New(c zrpc.Client) *Client {
+	return &Client{client: c}
+}
+
+
+func (x *Client) Hello() hello.Hello {
+	return hello.NewHello(x.client)
+}
