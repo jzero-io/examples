a0b1da63ce2fdc97d64dea8a1ca2c87ebc147d8e
jaron <jaron@jaronnie.com>
2024-08-02 05:02:10 +0000

    chore(simplerpc-goctl): update simplerpc-goctl

---
+++ simplerpc-goctl/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"simplerpc-goctl/internal/config"
+	"simplerpc-goctl/internal/server"
+	"simplerpc-goctl/internal/svc"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+var configFile = flag.String("f", "etc/etc.yaml", "set config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := server.RegisterZrpc(c, ctx)
+	defer s.Stop()
+
+	printBanner(c)
+	logx.Infof("Starting rpc server at %s...", c.ListenOn)
+	s.Start()
+}
+
+func printBanner(c config.Config) {
+	figure.NewColorFigure(c.Banner.Text, c.Banner.FontName, c.Banner.Color, true).Print()
+}
